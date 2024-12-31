0b5b81f39792d797d5ca2d759d70f16491852f4a
Perju Mircea-Stefan <118820391+Vexionix@users.noreply.github.com>
2024-12-31 19:34:23 +0200

    edit comments to fit the max length of line

--- database.py
+++ database.py
@@ -1,7 +1,8 @@
 import sqlite3
 
-# Create a SQLite database used for storing the scraped information from Wikipedia about countries.
-# A country is represented by a name, capital, population, density, area, neighbors, language, timezone and regime.
+# Create a SQLite database used for storing the scraped information from Wikipedia
+# about countries. A country is represented by a name, capital,
+# population, density, area, neighbors, language, timezone and regime.
 conn = sqlite3.connect('states_of_the_world.db')
 cursor = conn.cursor()
 cursor.execute('''CREATE TABLE IF NOT EXISTS countries (
